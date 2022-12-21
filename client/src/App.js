1f9a12244e7f7f77ec4686bd2b583b81a99b36ad
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-21 17:20:14 +0100

    App component whit routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import Loggin from './Components/Loggin/Loggin';
+import Home from './Components/Home/Home';
+import DogDetail from './Components/Dog/Dog';
+import CreateDog from './Components/CreateDog/CreateDog';
+import NotFound from './Components/NotFound/NotFound';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Loggin/>
+      <Switch>
+        
+        <Route exact path='/'>
+          <Loggin/>  
+        </Route>
+
+        <Route exact path='/home'>
+          <Home/>
+        </Route>
+
+        <Route exact path ='/create'>
+          <CreateDog/>
+        </Route> 
+
+        <Route exact path ='/dog/:id' component={DogDetail}/>
+        
+        <Route path="*" component={NotFound} />  
+
+      </Switch>     
     </div>
   );
 }
