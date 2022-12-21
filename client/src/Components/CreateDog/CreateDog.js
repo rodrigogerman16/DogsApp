3419352abbae3d80da1e7648d2995163f0846f6f
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-21 17:20:42 +0100

    CreateDog component created

---
+++ client/src/Components/CreateDog/CreateDog.js
@@ -0,0 +1,11 @@
+import style from './CreateDog.module.css'
+
+const CreateDog = () =>{
+    return(
+        <div>
+            CreateDog Component
+        </div>
+    )
+}
+
+export default CreateDog;
