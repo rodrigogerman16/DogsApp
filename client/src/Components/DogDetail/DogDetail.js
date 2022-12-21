a6e464ac2183e44d0dfa3969755d51553304f876
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-21 17:20:58 +0100

    DogDetail component Created

---
+++ client/src/Components/DogDetail/DogDetail.js
@@ -0,0 +1,11 @@
+import style from './DogDetail.module.css'
+
+const DogDetail = () =>{
+    return(
+        <div>
+            DogDetail Component
+        </div>
+    )
+}
+
+export default DogDetail;
