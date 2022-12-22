9738afe63c064d4211695224e415890853847e1e
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-22 22:27:36 +0100

    Deleted coments

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,19 +1,12 @@
 const { Router } = require('express');
 const dogsPath = require('./paths/dogsPath');
 const tempPath = require('./paths/tempPath');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
-
-
+const cors = require('cors')
 const router = Router();
+router.use(cors({origin: '*'}))
+
 router.use('/dogs', dogsPath)
 router.use('/temps', tempPath)
-
-
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
 
 module.exports = router;
