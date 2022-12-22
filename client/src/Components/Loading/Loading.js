1eb38c2a064821151e223c53c5f278fb81d0e510
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-22 21:57:50 +0100

    Loading Component created

---
+++ client/src/Components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import style from './Loading.module.css';
+
+const Loading = () =>{
+    return(
+        <div className={style.LoadingContainer}>
+
+        </div>
+    );
+};
+
+export default Loading;
