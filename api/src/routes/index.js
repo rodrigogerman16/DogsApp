6c870ad00478188e959cae69dfc3ca5fdedea6d4
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-26 02:12:59 +0100

    cors deleted

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,7 @@
 const { Router } = require('express');
 const dogsPath = require('./paths/dogsPath');
 const tempPath = require('./paths/tempPath');
-const cors = require('cors')
 const router = Router();
-router.use(cors({origin: '*'}))
 
 router.use('/dogs', dogsPath)
 router.use('/temps', tempPath)
