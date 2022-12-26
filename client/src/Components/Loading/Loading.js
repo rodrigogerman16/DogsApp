7b5d4db74cf7b5926dc2847bbd1511ffefbdde72
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-26 02:07:03 +0100

    Loading css and component

--- client/src/Components/Loading/Loading.js
+++ client/src/Components/Loading/Loading.js
@@ -1,9 +1,12 @@
 import style from './Loading.module.css';
+import loading from '../Img/loading.gif';
+import dogImg from '../Img/dog.png';
 
 const Loading = () =>{
     return(
         <div className={style.LoadingContainer}>
-
+            <img src={dogImg} alt='Dog Logo'/>
+            <img src={loading} alt='Loading'/>
         </div>
     );
 };
