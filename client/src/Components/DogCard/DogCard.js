5aeeea3e7cee619f8ba0e7f1ee1f70d5598eac20
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-12-26 02:06:41 +0100

    dogCard completed info

--- client/src/Components/DogCard/DogCard.js
+++ client/src/Components/DogCard/DogCard.js
@@ -2,21 +2,26 @@ import style from './DogCard.module.css'
 import imageSecondary from '../Img/dog.png'
 import { Link } from 'react-router-dom'
 
-const DogCard = ({name, temps, image, weightMin, weightMax, id}) =>{
+const DogCard = ({name, temperaments, image, weightMin, weightMax, id}) =>{
     return(
-        <Link to={`/dog/${id}`}>
+        
             <div className={style.DogCardContainer}>
-                <div>
+                <div className={style.DogCardImg}>
                     <img src={image ? image : imageSecondary} alt='Dog Profile'/>
                 </div>
-                <div>
-                    <p>{name}</p>
-                    {temps.forEach(temp => <p>{temp}</p>)}
-                    <p>{weightMin}</p>
-                    <p>{weightMax}</p>
+                <div className={style.DogCardDescription}>
+                <Link to={`/dog/${id}`}><h2>{name}</h2></Link>
+                <p>{temperaments && typeof temperaments[0] === 'object'? temperaments?.map(t=>(
+                 t.name + ', '
+                )):temperaments?.join(', ')}</p>
+                {console.log(temperaments)}
+                <div className={style.DogCardWeight}>
+                    <p>Min Weight: {weightMin}kg</p>
+                    <p>Max Weight: {weightMax}kg</p>
+                </div>                    
                 </div>           
             </div>
-        </Link>
+       
     )
 }
 
