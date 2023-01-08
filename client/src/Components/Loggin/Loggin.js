f431824313fbb81ac51fcf1310059b62055fde12
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-01-08 21:26:15 +0100

    typo

--- client/src/Components/Loggin/Loggin.js
+++ client/src/Components/Loggin/Loggin.js
@@ -1,6 +1,6 @@
 import style from './Loggin.module.css'
 import { Link } from 'react-router-dom'
-import image from '../Img/Loggin.jpg'
+import image from '../Img/Loggin.png'
 
 const Loggin = () =>{
     return(
@@ -10,8 +10,7 @@ const Loggin = () =>{
             </div>
             <div className={style.LogginDescription}>
                 <h2>Hi DogLover!</h2>
-                <p>Do you want to search dogs and know more about them?</p>
-                <p>This is the webpage for you</p>
+                <h4>Do you want to search dogs and know more about them?</h4>
                 <Link to='home'>JOIN</Link>
             </div>
         </div>  
