97a84b4bbdd14aa3988e50d99d5ab702aa7a1b52
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-01-10 20:18:02 +0100

    undo change

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const { DB_PORT } = process.env
+const { PORT } = process.env
 
 
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.DB_PORT, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at procces.env.PORT'); // eslint-disable-line no-console
   });
 });
