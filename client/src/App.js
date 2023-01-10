f13db094c6e4ff43aef0ac81d2c69909d6a73f5b
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-01-10 20:36:25 +0100

    typo

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import CreateDog from './Components/CreateDog/CreateDog';
 import NotFound from './Components/NotFound/NotFound';
 import { Switch, Route } from 'react-router-dom';
 import axios from 'axios';
-axios.defaults.baseURL = 'https://dogsapp-production.up.railway.app/';
+axios.defaults.baseURL = 'https://dogs-app-sandy.vercel.app/';
 
 function App() {
   return (
