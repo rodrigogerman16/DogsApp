392c75d5a4558e89f4e58a1c196b98e0071efb83
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-01-11 01:38:28 +0100

    minimal change in loggin css and typo

--- client/src/Components/Loggin/Loggin.js
+++ client/src/Components/Loggin/Loggin.js
@@ -9,8 +9,8 @@ const Loggin = () =>{
                 <img src={image} alt='Dogs'/>  
             </div>
             <div className={style.LogginDescription}>
-                <h2>Hi DogLover!</h2>
-                <h4>Do you want to search dogs and know more about them?</h4>
+                <h2>WoofFinder</h2>
+                <h4>Search, create and know more about them!</h4>
                 <Link to='home'>JOIN</Link>
             </div>
         </div>  
